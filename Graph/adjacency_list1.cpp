8d357653524cb31e088706cc527e59cb85f0c16b
Avishek kumar <46109905+avishekkumar0204@users.noreply.github.com>
2020-12-30 18:39:40 +0530

    Create adjacency_list1.cpp

---
+++ Graph/adjacency_list1.cpp
@@ -0,0 +1,41 @@
+//if it is given that edge are bi-directional and no weightage are there between two nodes
+
+
+#include<bits/stdc++.h>
+using namespace std;
+class Graph
+{
+    int v;
+    map<int,list<int>> m;
+    public:
+        Graph(int v)
+        {
+            this->v=v;
+        }
+        void addEdge(int x,int y)
+        {
+            m[x].push_back(y);
+            m[y].push_back(x);
+        }
+        void printGraph()
+        {
+            for(auto it=m.begin();it!=m.end();it++)
+            {
+                list<int> l=it->second;
+                cout<<it->first<<"->";
+                for(auto e:l)
+                    cout<<e<<" ";
+                cout<<endl;
+
+            }
+        }
+};
+int main()
+{
+    Graph g;
+    g.addEdge(0,1);
+    g.addEdge(0,2);
+    g.addEdge(1,2);
+    g.addEdge(2,3);
+    g.printGraph();
+}
